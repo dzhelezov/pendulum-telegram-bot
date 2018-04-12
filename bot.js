faf18c79747700afe966ca317eae4d94d4deb285
ofo42 <of@hlx.ai>
2018-04-12 11:25:05 +0200

    fixes

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Bot = require('node-telegram-bot-api');
 const IOTA = require('iota.lib.js');
+const config = requier('./config.json');
 
 const provider = 'http://hlxbox.net:14700';
-const token = '594517291:AAFU0UV0GfLMFpAYGDr6-VNeCK4eFDu6q1k';
+const token = config.token;
 const trigger = 'getNodeInfo';
 
 var iota = new IOTA({
@@ -11,23 +12,29 @@ var iota = new IOTA({
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new Bot(token, {polling: true});
 
-function parseResponse(lssm, lm, t, n) {
-  return "Latest Solid Subtangle Milestone Index: " + lssm + '\n' +
-         "Latest Milestone Index: " + lm + '\n' +
-         "Tips: " + t + '\n' +
-         "Neighbors: " + n + '\n';
-}
+
 
 bot.on('message', (msg) => {
-  const prov = iota.provider;
   const chatId = msg.chat.id;
   var cmd = msg.text.toString();
-  bot.sendMessage(chatId, 'Request: ' + cmd + ' received...');
+  bot.sendMessage(chatId, 'Request: ' + cmd + ' received.');
 
-  if (cmd != '/start') {
-    bot.sendMessage(chatId, 'Connected to: ' + prov);
+  /**
+  *
+  * help Commands
+  */
+
+  if (msg.text.toString() === '/help' || '/start') {
+    bot.sendMessage(printUsage());
   }
+
+  /**
+  *
+  * API Commands
+  */
+
   if (msg.text.toString() === '/getNodeInfo') {
+      req();
       iota.api.getNodeInfo(function(e, response) {
         var lssm = response.latestSolidSubtangleMilestoneIndex;
         var lm = response.latestMilestoneIndex;
@@ -35,6 +42,44 @@ bot.on('message', (msg) => {
         var neighbors = response.neighbors;
         resp = parseResponse(lssm, lm, tips, neighbors);
         bot.sendMessage(chatId, resp);
+        console.log("'/getNodeInfo' request performed.");
       });
   }
+
+  if (msg.text.toString() === '/getTips') {
+    req();
+    iota.api.getTips(function(e, r) {
+        if (!e) {
+          var recentTips = r.slice(0,10);
+          bot.sendMessage(chatId, "Latest Tips:" + '\n' + recentTips);
+        }
+        else {
+          bot.sendMessage(chatId, "Error: " + '\n' + e);
+        }
+        console.log("'/getTips' request performed.");
+    });
+  }
 });
+
+/**
+*
+* Helper Functions
+*/
+
+
+function printUsage() {
+  return "Available Commands: " + '\n' +
+         "/getNodeInfo" + '\n' +
+         "/getTips" + '\n';
+}
+
+function parseNodeInfo(lssm, lm, t, n) {
+  return "Latest Solid Subtangle Milestone Index: " + lssm + '\n' +
+         "Latest Milestone Index: " + lm + '\n' +
+         "Tips: " + t + '\n' +
+         "Neighbors: " + n + '\n';
+}
+
+function req() {
+  bot.sendMessage("Request '/getNodeInfo' sent to: " + iota.provider);
+}
