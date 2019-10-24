3b604270e0366f9fa420014f97faff4effadb140
dzhelezov <dzhelezov@gmail.com>
2019-10-24 16:49:47 +0300

    timeout promise + refactoring in index

--- index.js
+++ index.js
@@ -11,16 +11,35 @@ exports.handler = function (event, context, callback) {
     };
     console.log(data);
     console.log(data.message);
-    bot.handleBotMessage(data.message)
-        .then(() => {
-          res.body = "OK";
-          callback(null, res)
-        })
-        .catch((err) => {
-          // we still send 200
-          // to prevent repeated webhook calls
-          res.body = `Error: ${err}`;
-          callback(null, res)
-        });
+    // set timeout for 5 seconds
+    let handleMessage = promiseTimeout(5000, bot.handleBotMessage(data.message));
 
+    handleMessage.then(response => {
+        res.body = JSON.stringify(response);
+        callback(null, res);
+    }).catch(err => {
+        // we still return 200 to prevent
+        // telegram from resubmitting
+        // the message. But we log the error
+        console.error(err);
+        res.body = `Error: ${err}`;
+        callback(null, res);
+    })
 };
+
+function promiseTimeout(ms, promise) {
+  let id;
+  let timeout = new Promise((resolve, reject) => {
+    id = setTimeout(() => {
+      reject('Timed out in ' + ms + 'ms.')
+    }, ms)
+  })
+
+  return Promise.race([
+    promise,
+    timeout
+  ]).then((result) => {
+    clearTimeout(id)
+    return result
+  })
+}
