804ae340e456d9bd52d4879600f46fde8629d3e3
dzhelezov <dzhelezov@gmail.com>
2019-10-24 14:46:30 +0300

    refectoring and stats

--- gsheets.js
+++ gsheets.js
@@ -38,53 +38,55 @@ const RANGE_REGEX = /\!A([0-9]+):/g
  * See the defaultauth.js sample for an alternate way of fetching compute credentials.
  */
 
-async function getRows(indexes, sheet) {
-  const client = await auth.getClient();
-  var ranges = [];
+async function get(ranges) {
+    const client = await auth.getClient();
+    var request = {
+       spreadsheetId,
+       ranges,
+       valueRenderOption: 'UNFORMATTED_VALUE',
+       auth: client
+    };
 
-  indexes.map(i => {
-     var r = (sheet) ? `${sheet}!A${i}:B${i}` : `A${i}:B${i}`;
-     ranges.push(r);
-  })
-
-  var request = {
-     spreadsheetId,
-     ranges,
-     valueRenderOption: 'UNFORMATTED_VALUE',
-     auth: client
-  };
-
-  return new Promise((resolve, reject) => {
-     sheets.spreadsheets.values.batchGet(request, (err, response) => {
-        if (err) {
-            reject(err)
-            return
-        }
-
-        var toReturn = [];
-        console.log(response.data.valueRanges);
-        response.data.valueRanges.map(r => {
-            var address = r.values[0][0];
-            var seed = r.values[0][1];
-
-            //var parse = RANGE_REGEX.exec(''+r.range);
-            //if (parse === null || parse[1] === null) {
-            //   reject("Can't parse the range");
-            //   return
-            //}
-            // can't get the godamn regex working, doing split
-            var cell = r.range.split("!")[1].split(":")[0];
-            // this is the cell to where the updates for this row
-            // should be written
-            var updateCell = cell.replace("A", "C");
-
-            toReturn.push({
-                address, seed, updateCell
-            });
+    return new Promise((resolve, reject) => {
+        sheets.spreadsheets.values.batchGet(request, (err, response) => {
+            if (err) {
+                reject(err)
+                return
+            }
+            
+            resolve(response.data.valueRanges);
         })
-        resolve(toReturn);
-     })
-  });
+    })
+}
+
+async function getStats() {
+    var ranges = 'D2:E2';
+    var values = await get(ranges);
+    return {
+        available: parseInt(values[0].values[0][0]),
+        total: parseInt(values[0].values[0][1])
+    }
+}
+
+async function getRows(indexes, sheet) {
+    var ranges = indexes.map(i => {
+        return (sheet) ? `${sheet}!A${i}:C${i}` : `A${i}:C${i}`
+    })
+
+    var valueRanges = await get(ranges);
+    return valueRanges.map(r => {
+
+       var cell = r.range.split("!")[1].split(":")[0];
+       var updateCell = cell.replace("A", "C");
+       return {
+           address: r.values[0][0],
+           seed: r.values[0][1],
+           spent: (r.values[0][2]) ? r.values[0][2] : null,
+           available: !r.values[0][2],
+           updateCell
+       }
+    })
+
 }
 
 async function updateSheet(cell, value, sheet) {
@@ -95,15 +97,15 @@ async function updateSheet(cell, value, sheet) {
   var range = (sheet) ? `${sheet}!${cell}` : cell;
 
   var request = {
-    spreadsheetId,
-    range: range,
-    valueInputOption: 'RAW',
-    resource: {
-      values: [
-          [value]
-      ]
-    },
-    auth: client,
+      spreadsheetId,
+      range: range,
+      valueInputOption: 'RAW',
+      resource: {
+        values: [
+            [value]
+        ]
+      },
+      auth: client,
   };
 
 
@@ -121,6 +123,9 @@ async function updateSheet(cell, value, sheet) {
 }
 
 if (module === require.main) {
+  getStats().
+    then((response) => console.log(JSON.stringify(response))).
+    catch((err) => console.error(err));
   getRows([2332, 124, 34, 3, 5]).
     then((response) => console.log(JSON.stringify(response))).
     catch((err) => console.error(err));
@@ -130,5 +135,5 @@ if (module === require.main) {
 }
 
 module.exports = {
-  updateSheet, getRows
+  updateSheet, getRows, get, getStats
 };
